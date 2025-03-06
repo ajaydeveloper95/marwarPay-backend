7cd545669dad3795b92bea9f61760b7be5cce9e2
ajay <ajayasrdeveloper1@gmail.com>
2025-03-06 16:39:44 +0530

    flipzik corn active

--- src/utils/scheduleTask.js
+++ src/utils/scheduleTask.js
@@ -312,10 +312,10 @@ async function processWaayuPayOutFnMindMatrix(item, indexNumber) {
 
 let tempTrxIds = []
 function scheduleFlipzikImpactPeek() {
-    cron.schedule('*/2 * * * *', async () => {
+    cron.schedule('*/30 * * * * *', async () => {
         const release = await transactionMutexImpactFlipZik.acquire();
         const threeHoursAgo = new Date();
-        threeHoursAgo.setHours(threeHoursAgo.getHours() - 3)
+        threeHoursAgo.setHours(threeHoursAgo.getHours() - 1)
 
         let GetData = await payOutModelGenerate.find({
             isSuccess: "Pending",
@@ -323,14 +323,14 @@ function scheduleFlipzikImpactPeek() {
             createdAt: { $lt: threeHoursAgo },
             pannelUse: "flipzikPayoutImpactPeek"
         })
-            .sort({ createdAt: 1 }).limit(5)
+            .sort({ createdAt: 1 }).limit(25)
 
         try {
             if (GetData?.length !== 0) {
                 GetData.forEach(async (item) => {
                     tempTrxIds.push(item?.trxId)
-                    console.log(item)
-                    // await processFlipzikPayout(item)
+                    // console.log(item)
+                    await processFlipzikPayout(item)
                 })
             } else {
                 console.log("No Pending Found In Range !")
@@ -1577,7 +1577,7 @@ export default function scheduleTask() {
     // payoutTaskScript()
     // payoutDeductPackageTaskScript()
     // payinScheduleTask2()
-    // scheduleFlipzikImpactPeek()
+    scheduleFlipzikImpactPeek()
     // EwalletManuplation()
     // payOutDuplicateEntryRemove()
 }
