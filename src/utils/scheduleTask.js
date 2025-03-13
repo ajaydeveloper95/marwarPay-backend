b89e90819039cc700034645c8e5d4d46cf1a4501
ajay <ajayasrdeveloper1@gmail.com>
2025-03-13 14:28:14 +0530

    waayupay clear

--- src/utils/scheduleTask.js
+++ src/utils/scheduleTask.js
@@ -37,7 +37,7 @@ const loopMutex = new Mutex();
 
 let trxIdsToAvoid = []
 function scheduleWayuPayOutCheckSecond() {
-    cron.schedule('*/3 * * * *', async () => {
+    cron.schedule('*/30 * * * * *', async () => {
         const release = await transactionMutexImpactPeek.acquire();
         const threeHoursAgo = new Date();
         threeHoursAgo.setHours(threeHoursAgo.getHours() - 2)
@@ -47,7 +47,7 @@ function scheduleWayuPayOutCheckSecond() {
             createdAt: { $lt: threeHoursAgo },
             trxId: { $nin: trxIdsToAvoid }
         })
-            .sort({ createdAt: 1 }).limit(10)
+            .sort({ createdAt: 1 }).limit(40)
         try {
             if (GetData?.length !== 0) {
                 GetData.forEach(async (item) => {
@@ -1625,7 +1625,7 @@ export default function scheduleTask() {
     // payoutTaskScript()
     // payoutDeductPackageTaskScript()
     // payinScheduleTask2()
-    scheduleFlipzikImpactPeek()
+    // scheduleFlipzikImpactPeek()
     // EwalletManuplation()
     // payOutDuplicateEntryRemove()
 }
