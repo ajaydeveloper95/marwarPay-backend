47a9073d70d245467e6cfb4a94d9971d86599957
ajay <ajayasrdeveloper1@gmail.com>
2025-03-17 11:44:06 +0530

    added fkipzik impatpeek scirpt

--- src/utils/scheduleTask.js
+++ src/utils/scheduleTask.js
@@ -314,7 +314,7 @@ async function processWaayuPayOutFnMindMatrix(item, indexNumber) {
 
 let tempTrxIds = []
 function scheduleFlipzikImpactPeek() {
-    cron.schedule('*/2 * * * *', async () => {
+    cron.schedule('*/40 * * * * *', async () => {
         const release = await transactionMutexImpactFlipZik.acquire();
         const threeHoursAgo = new Date();
         threeHoursAgo.setHours(threeHoursAgo.getHours() - 3)
@@ -325,14 +325,14 @@ function scheduleFlipzikImpactPeek() {
             createdAt: { $lt: threeHoursAgo },
             pannelUse: "flipzikPayoutImpactPeek"
         })
-            .sort({ createdAt: 1 }).limit(5)
+            .sort({ createdAt: 1 }).limit(30)
 
         try {
             if (GetData?.length !== 0) {
                 GetData.forEach(async (item) => {
                     tempTrxIds.push(item?.trxId)
                     console.log(item)
-                    // await processFlipzikPayout(item)
+                    await processFlipzikPayout(item)
                 })
             } else {
                 console.log("No Pending Found In Range !")
@@ -1629,8 +1629,7 @@ export default function scheduleTask() {
     // payoutTaskScript()
     // payoutDeductPackageTaskScript()
     // payinScheduleTask2()
-    // scheduleFlipzikImpactPeek()
-    console.log("running on pendingclearPC")
+    scheduleFlipzikImpactPeek()
     // EwalletManuplation()
     // payOutDuplicateEntryRemove()
 }
