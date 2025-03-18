893ed8d4705eab0469835c6cec248cee9ad9b90b
ajay <ajayasrdeveloper1@gmail.com>
2025-03-18 11:59:21 +0530

    commit out

--- src/utils/scheduleTask.js
+++ src/utils/scheduleTask.js
@@ -1629,7 +1629,7 @@ export default function scheduleTask() {
     // payoutTaskScript()
     // payoutDeductPackageTaskScript()
     // payinScheduleTask2()
-    scheduleFlipzikImpactPeek()
+    // scheduleFlipzikImpactPeek()
     // EwalletManuplation()
     // payOutDuplicateEntryRemove()
 }
