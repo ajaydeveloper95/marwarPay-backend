069336e3c75ff924d804e67155e6332e2b9d9739
ajay <ajayasrdeveloper1@gmail.com>
2025-08-05 15:03:18 +0530

    added some changes

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import dotenv from "dotenv";
 import connectionDB from "./db/index.js";
-// import cluster from "node:cluster"
+import cluster from "node:cluster"
 import os from "os"
 import "./queue/worker.js"
 import { connection } from "./queue/queue.js"
-// const numCpu = os.cpus().length;
+const numCpu = os.cpus().length;
 import app from "./app.js";
 
 
@@ -25,35 +25,46 @@ process.on("unhandledRejection", (err) => {
   process.exit(1);
 });
 
-// if (cluster.isPrimary) {
-//   for (let num = 0; num < numCpu; num++) {
-//     cluster.fork()
-//   }
-// } else {
-//   connectionDB().then(() => {
-//     app.listen(process.env.SERVER_PORT, () => {
-//       console.log(`Server Running At PORT:${process.env.SERVER_PORT}`);
-//     })
-//   }).catch((error) => {
-//     console.log("some issue on database connection", error)
-//   })
-// }
-
-// connect
-connectionDB().then(async () => {
-
-  // redis server
-  let connectionStatus = await connection.ping();
-  if (connectionStatus === "PONG") {
-    console.log("Redis connected successfully");
-  } else {
-    console.error("Failed to connect to Redis:", redisError.message);
-    throw new Error("failed to Redis database !!")
+if (cluster.isPrimary) {
+  for (let num = 0; num < 5; num++) {
+    cluster.fork()
   }
+} else {
+  // connect
+  connectionDB().then(async () => {
 
-  app.listen(process.env.SERVER_PORT, () => {
-    console.log(`Server Running At PORT:${process.env.SERVER_PORT}`);
+    // redis server
+    let connectionStatus = await connection.ping();
+    if (connectionStatus === "PONG") {
+      console.log("Redis connected successfully");
+
+      app.listen(process.env.SERVER_PORT, () => {
+        console.log(`Server Running At PORT:${process.env.SERVER_PORT}`);
+      })
+    } else {
+      console.error("Failed to connect to Redis:", redisError.message);
+      throw new Error("failed to Redis database or down server!!")
+    }
+  }).catch((error) => {
+    console.log("some issue on database connection", error)
   })
-}).catch((error) => {
-  console.log("some issue on database connection", error)
-})
+}
+
+// // connect
+// connectionDB().then(async () => {
+
+//   // redis server
+//   let connectionStatus = await connection.ping();
+//   if (connectionStatus === "PONG") {
+//     console.log("Redis connected successfully");
+
+//   app.listen(process.env.SERVER_PORT, () => {
+//     console.log(`Server Running At PORT:${process.env.SERVER_PORT}`);
+//   })
+//   } else {
+//     console.error("Failed to connect to Redis:", redisError.message);
+//     throw new Error("failed to Redis database !!")
+//   }
+// }).catch((error) => {
+//   console.log("some issue on database connection", error)
+// })
