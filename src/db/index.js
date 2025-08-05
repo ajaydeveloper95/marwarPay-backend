013d205e3b56d9f5e19b233e0b57877bc59b3fe2
ajay <ajayasrdeveloper1@gmail.com>
2025-08-05 17:14:11 +0530

    added

--- src/db/index.js
+++ src/db/index.js
@@ -3,7 +3,10 @@ import { DB_Name } from "../constants.js";
 
 const connectionDB = async () => {
     try {
-        let connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_Name}`);
+        let connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_Name}`, {
+            maxPoolSize: 10, // adjust for your workload
+            serverSelectionTimeoutMS: 5000, // shorter fail fast
+        });
         console.log(`\n MongoDB connected !! DB Host : ${connectionInstance.connection.host}`)
     } catch (error) {
         console.log("Some issue on Database connection !! Further action required !!")
