12e1a188b0cbab3d0f0940e2ddd05742052231f7
ajay <ajayasrdeveloper1@gmail.com>
2025-08-07 13:23:44 +0530

    fix

---
+++ src/jobs/eWallet.jobs.js
@@ -0,0 +1,32 @@
+import { eWalletQueue } from "../queue/queue.js";
+
+
+export const eWalletCrJobs = async (memberId, transactionAmount, chargeAmount, txnID) => {
+    const ewalletQueueData = await eWalletQueue.add("eWalletAdded", {
+        memberId, transactionType: "Cr.", transactionAmount, chargeAmount, txnID
+    }, {
+        attempts: 3,
+        backoff: {
+            type: 'exponential',
+            delay: 5000 // 5 seconds
+        },
+        removeOnComplete: true
+    })
+
+    return "Success Added your Job"
+}
+
+export const eWalletDrJobs = async (memberId, transactionAmount, chargeAmount, txnID) => {
+    const ewalletQueueData = await eWalletQueue.add("eWalletAdded", {
+        memberId, transactionType: "Dr.", transactionAmount, chargeAmount, txnID
+    }, {
+        attempts: 3,
+        backoff: {
+            type: 'exponential',
+            delay: 5000 // 5 seconds
+        },
+        removeOnComplete: true
+    })
+
+    return "Success Added your Job"
+}
--- src/queue/worker.js
+++ src/queue/worker.js
@@ -1,6 +1,7 @@
 import { Worker } from 'bullmq';
 import { connection } from "./queue.js"
 import upiWalletModel from "../models/upiWallet.model.js";
+import eWalletModel from "../models/Ewallet.model.js";
 import userDB from '../models/user.model.js';
 
 const upiWalletWorker = new Worker("upiWallet", async job => {
@@ -66,4 +67,100 @@ upiWalletWorker.on('failed', (jobId, failedReason) => {
     null
 });
 
-export { upiWalletWorker };
+const eWalletWorker = new Worker("eWallet", async job => {
+
+    // start
+    const { memberId, transactionType, transactionAmount, chargeAmount, txnID } = job?.data
+
+    // db locking with deducted amount 
+    const walletDucdsession = await userDB.startSession();
+    const transactionOptions = {
+        readConcern: { level: 'linearizable' },
+        writeConcern: { w: 'majority' },
+        readPreference: { mode: 'primary' },
+        maxTimeMS: 2000
+    };
+    // wallet deducted and store ewallet trx
+    try {
+        walletDucdsession.startTransaction(transactionOptions);
+        const opts = { walletDucdsession };
+
+        if (transactionType === "Cr.") {
+            // update wallet Cr.
+            let userWallet = await userDB.findByIdAndUpdate(memberId, { $inc: { EwalletBalance: + transactionAmount, EwalletFundLock: + transactionAmount } }, {
+                returnDocument: 'after',
+                walletDucdsession
+            })
+
+            let afterAmount = userWallet?.EwalletBalance
+            let beforeAmount = userWallet?.EwalletBalance - transactionAmount;
+
+            // ewallet store 
+            let walletModelDataStore = {
+                memberId: user?._id,
+                transactionType: "Cr.",
+                transactionAmount: transactionAmount,
+                beforeAmount: beforeAmount,
+                chargeAmount: chargeAmount,
+                afterAmount: afterAmount,
+                description: `Successfully Dr. amount: ${Number(transactionAmount)} with transaction Id: ${txnID}`,
+                transactionStatus: "Success",
+            }
+
+            await eWalletModel.create([walletModelDataStore], opts)
+            // Commit the transaction
+            await walletDucdsession.commitTransaction();
+        } else if (transactionType === "Dr.") {
+            // update wallet 
+            let userWallet = await userDB.findByIdAndUpdate(memberId, { $inc: { EwalletBalance: - transactionAmount, EwalletFundLock: - transactionAmount } }, {
+                returnDocument: 'after',
+                walletDucdsession
+            })
+
+            let afterAmount = userWallet?.EwalletBalance
+            let beforeAmount = userWallet?.EwalletBalance + transactionAmount;
+
+            // ewallet store 
+            let walletModelDataStore = {
+                memberId: user?._id,
+                transactionType: "Dr.",
+                transactionAmount: transactionAmount,
+                beforeAmount: beforeAmount,
+                chargeAmount: chargeAmount,
+                afterAmount: afterAmount,
+                description: `Successfully Dr. amount: ${Number(transactionAmount)} with transaction Id: ${txnID}`,
+                transactionStatus: "Success",
+            }
+
+            await eWalletModel.create([walletModelDataStore], opts)
+            // Commit the transaction
+            await walletDucdsession.commitTransaction();
+        } else {
+            null
+        }
+        // console.log('Transaction committed successfully');
+    } catch (error) {
+        // console.log(error)
+        await walletDucdsession.abortTransaction();
+    }
+    finally {
+        walletDucdsession.endSession();
+    }
+    // db locking end
+
+}, { concurrency: 1, connection, prefix: 'zanithpay-backend' })
+
+eWalletWorker.on('completed', (jobId) => {
+    // console.log(jobId)
+    // console.log(`✅ Job ${jobId?.data} completed`);
+    null
+});
+
+// Listen to job failure
+eWalletWorker.on('failed', (jobId, failedReason) => {
+    // console.log(jobId?.data, jobId, "data")
+    // console.error(`❌ Job ${jobId?.data} failed: ${failedReason}`);
+    null
+});
+
+export { upiWalletWorker, eWalletWorker };
